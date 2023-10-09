9dfc12e8239cfd864b9ad1b33615643b8c871510
Hernan Rodrigo Bustamante <bustamanterodrigo15@gmail.com>
2023-10-09 20:25:15 -0300

    configurando el boton

--- tematica.js
+++ tematica.js
@@ -2,10 +2,9 @@ var boton = document.getElementById("caja-boton")
 var body = document.querySelector("body")
 var titulo = document.querySelector(".main-titulo")
 var footer = document.querySelector("footer")
-var h3 = document.querySelector(".titulo-categoria")
+var categoria = document.querySelector(".main")
 var cabecera = document.querySelector(".cabecera")
 var navlinks = document.querySelector(".links")
-
 
 // titulo-categoria
 
@@ -14,9 +13,10 @@ boton.onclick = function(){
     body.classList.toggle('activar')
     titulo.classList.toggle('titulo-oscuro')
     footer.classList.toggle('footer-oscuro')
-    h3.classList.toggle('titulo-h3')
+    categoria.classList.toggle('main-activar')
     cabecera.classList.toggle('cabecera-oscuro')
     navlinks.classList.toggle('nav-oscuros')
+
 
     
 
