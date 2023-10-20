4831288bdcc4d12bbfd4883b96d0e9bb7cfcc4a9
Hernan Rodrigo Bustamante <bustamanterodrigo15@gmail.com>
2023-10-20 16:06:42 -0300

    local storage agregado a ambos botones en productos

--- js/boton-tematico.js
+++ js/boton-tematico.js
@@ -4,4 +4,19 @@ var cuerpo = document.querySelector(".cuerpo")
 boton.onclick = function(){
     boton.classList.toggle('activar')
     cuerpo.classList.toggle("bg-alter")
+
+    if(document.getElementById("caja-boton").classList.contains("activar")){
+        localStorage.setItem("boton-oscuro", "true");
+    }   else{
+        localStorage.setItem("boton-oscuro", "false");
+
+    }
+}
+
+if(localStorage.getItem("boton-oscuro") === "true"){
+    boton.classList.add('activar')
+    cuerpo.classList.add("bg-alter")
+} else{
+    boton.classList.remove('activar')
+    cuerpo.classList.remove("bg-alter")
 }
--- js/menu.js
+++ js/menu.js
@@ -11,5 +11,26 @@ function animateBar(){
     line2.classList.toggle("line2-active");
     line3.classList.toggle("line3-active");
     nav.classList.toggle("nav-activo")
+    // if(document.querySelector(".nav").contains("nav-activo")){
+    //     localStorage.setItem("tema-oscuro", "true")
+    // }
+    if(document.getElementById("nav").classList.contains("nav-activo")){
+        localStorage.setItem("menu-barra", "true");
+
+    } else{
+        localStorage.setItem("menu-barra", "false");
+    }
+}
+
+if(localStorage.getItem("menu-barra") === "true"){
+    nav.classList.add("nav-activo")
+    line1.classList.add("line1-active");
+    line2.classList.add("line2-active");
+    line3.classList.add("line3-active");
+}   else{
+    nav.classList.remove("nav-activo")
+    line1.classList.remove("line1-active");
+    line2.classList.remove("line2-active");
+    line3.classList.remove("line3-active");
 }
 
