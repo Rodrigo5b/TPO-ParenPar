2874b91626b4f14489a7b57fee7df7afb5cae056
Victoria <Victoriabrambilla@outlook.es>
2023-10-24 16:30:32 -0300

    actualizacion3

--- js/boton-tematico.js
+++ js/boton-tematico.js
@@ -1,9 +1,10 @@
 var boton = document.getElementById("caja-boton")
 var cuerpo = document.querySelector(".cuerpo")
+var form =document.querySelector(".formulario")
 boton.onclick = function(){
     boton.classList.toggle('activar')
     cuerpo.classList.toggle("bg-alter")
-
+    form.classList.toggle('formoscuro')
     if(document.getElementById("caja-boton").classList.contains("activar")){
         localStorage.setItem("boton-oscuro", "true");
     }   else{
@@ -13,7 +14,10 @@ boton.onclick = function(){
 if(localStorage.getItem("boton-oscuro") === "true"){
     boton.classList.add('activar')
     cuerpo.classList.add("bg-alter")
+    form.classList.add('formoscuro')
 } else{
     boton.classList.remove('activar')
     cuerpo.classList.remove("bg-alter")
+    form.classList.remove('formoscuro')
+
 }
